2204eeb280318f5005cf4c900a9de73619efba35
vmpyre <umarsalmanrao5@gmail.com>
2021-12-21 03:39:53 -0500

    Update discord_crypto_price_bot.py
    Added Error Handling for common exceptions

--- src/discord_crypto_price_bot.py
+++ src/discord_crypto_price_bot.py
@@ -8,11 +8,11 @@ DISCORD_BOT_TOKEN = "ADD DISCORD BOT TOKEN HERE"
 client = discord.Client()
 
 # Function that fetches live crypto token price from the MEXC exchange.
-def token_price(ticker):
+def token_price(ticker = 'CHICKS'):
     mrkt_url = 'http://mexc.com/open/api/v2/market/ticker'
     ticker1 = ticker
     ticker2 = 'USDT'
-    
+
     try:
         response = requests.get(url=mrkt_url, params={'symbol':ticker1+'_'+ticker2})
 	
@@ -28,17 +28,25 @@ def token_price(ticker):
         print("IndexError: " + str(error))
 
 # Runs the token_price() function and updates the price as the bot status every 5 seconds.
-@tasks.loop(seconds = 5)
+@tasks.loop(seconds=5)
 async def switch_presence():
-    # Change the token name below to pull the live price
-    # Right now, it's set to pull price of the token 'CHICKS'
-    current_price = token_price('CHICKS')
+    current_price = token_price()
+    if current_price is not None: 
+        try:
+            activity = discord.Game(name='$'+ current_price, type=3)
+            await client.change_presence(status=discord.Status.online, activity=activity)
+
+        except Exception as e:
+            print("ERROR LOGGING - Status change fail: " + str(e))
     
-    activity = discord.Game(name='$'+ current_price, type=3)
-    await client.change_presence(status=discord.Status.online, activity=activity)
+    else:
+        print("ERROR LOGGING - current_price is NoneType")
 
 @client.event
 async def on_ready():
-    switch_presence.start()
+    try:
+        switch_presence.start()
+    except Exception as e:
+        print("ERROR LOGGING - Status change fail: " + str(e))
 
 client.run(DISCORD_BOT_TOKEN)
